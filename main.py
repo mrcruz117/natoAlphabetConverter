7eadf8a47978513bd3858e3fd5d3a2a6bfb07f08
mrcruz117 <mrcruz1989@gmail.com>
2022-01-21 14:50:59 -0500

    returning coded word as nato words

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 student_dict = {
-    "student": ["Angela", "James", "Lily"], 
+    "student": ["Angela", "James", "Lily"],
     "score": [56, 76, 98]
 }
 
-#Looping through dictionaries:
+# Looping through dictionaries:
 for (key, value) in student_dict.items():
-    #Access key and value
+    # Access key and value
     pass
 
 import pandas
+
 student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
+# Loop through rows of a data frame
 for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
+    # Access index and row
+    # Access row.student or row.score
     pass
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+# TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+nato_dict = pandas.read_csv('nato_phonetic_alphabet.csv', index_col=0, squeeze=True).to_dict()
+print(nato_dict)
 
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def convert_to_nato(word):
+    converted = [nato_dict[f"{letter.upper()}"] for letter in word]
+    return converted
+
+
+result = convert_to_nato("pizza")
+
+print(result)
